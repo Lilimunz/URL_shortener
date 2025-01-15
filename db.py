b7466635ffc6be4431bfd97afc4b96dcc2375f8a
Alicia <alicia.muniz@gestaopublica.etc.br>
2025-01-15 17:35:14 -0300

    refactor database to file

--- db.py
+++ db.py
@@ -1,9 +1,22 @@
-DATABASE = {}
+import json
 
+PATH = 'database.json'
+
+def read_database() -> dict:
+    with open(PATH) as file:
+        return json.load(file)
+
+def write_database(data: dict) -> dict:
+    with open(PATH, 'w') as file:
+        json.dump(data, file)
+        return data
 
 def create(id: str, url: str) -> str:
-    DATABASE[id] = url
+    database = read_database()
+    database[id] = url
+    write_database(database)
     return id
 
 def find(id: str) -> str | None:
-    return DATABASE.get(id)
+    database = read_database()
+    return database.get(id)
