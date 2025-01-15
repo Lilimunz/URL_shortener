c524fa5f2fcc62df55ba9d3ae899498e6672fe71
Alicia <alicia.muniz@gestaopublica.etc.br>
2025-01-15 17:35:15 -0300

    add fastapi routes

---
+++ api.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+import starlette.status as status
+
+import core
+
+
+app = FastAPI()
+
+@app.post("/create")
+def create_url(url):
+    id = core.create_url(url)
+    return {
+        'url': url,
+        'id': id
+    }
+
+@app.get("/{id}")
+def search_url(id):
+    url = core.search_url(id)
+    if "http" not in url:
+        url = f"http://{url}"
+    return RedirectResponse(url=url, status_code=status.HTTP_303_SEE_OTHER)
